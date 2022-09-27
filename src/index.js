f83745be3b3a67aab31dd496b257ed756e57a56e
robertvoss <vossrobertt@gmail.com>
2022-09-27 20:33:43 -0300

    react-course

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+ReactDOM.render(
+    <div id="app">
+        <Primeiro></Primeiro>
+        <ComParametro
+            titulo ="Situaçao Do Aluno"
+            aluno ="Pedro" 
+            nota={9.3} />
+        <Fragmento />    
+    </div>,
+    document.getElementById('root')
+)
